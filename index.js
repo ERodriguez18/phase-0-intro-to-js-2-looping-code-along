ae000a146c94a155356e1978d533829cb369cd0a
Erick <erisrnb18@gmail.com>
2021-03-19 12:58:37 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName){
+    let thankYouCards=[];
+    for (let i=0; i<namesArray.length; i++){
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(number){
+    var i= number
+    while (i != -1) {
+        console.log(i);
+        i--;
+    }
+
+}
